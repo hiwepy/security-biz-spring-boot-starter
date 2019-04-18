00d0bf0eb8a12cb1cab862532d2d7ed068aee21e
wandl <hnxyhcwdl1003@163.com>
2019-04-18 13:22:50 +0800

    优化逻辑

---
+++ src/main/java/org/springframework/security/boot/biz/property/SessionFixationPolicy.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2018, vindell (https://github.com/vindell).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.security.boot.biz.property;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Session Fixation Policy 
+ * @author ： <a href="https://github.com/vindell">wandl</a>
+ */
+public enum SessionFixationPolicy {
+
+	/**
+	 * Specifies that the Servlet container-provided session fixation protection
+	 * should be used. When a session authenticates, the Servlet 3.1 method
+	 * {@code HttpServletRequest#changeSessionId()} is called to change the session
+	 * ID and retain all session attributes. Using this option in a Servlet 3.0 or
+	 * older container results in an {@link IllegalStateException}.
+	 */
+	CHANGE_SESSION_ID,
+	/**
+	 * Specifies that a new session should be created and the session attributes
+	 * from the original {@link HttpSession} should be retained.
+	 */
+	MIGRATE_SESSION,
+	/**
+	 * Specifies that a new session should be created, but the session attributes
+	 * from the original {@link HttpSession} should not be retained.
+	 */
+	NEW_SESSION,
+	/**
+	 * Specifies that no session fixation protection should be enabled. This may be
+	 * useful when utilizing other mechanisms for protecting against session
+	 * fixation. For example, if application container session fixation protection
+	 * is already in use. Otherwise, this option is not recommended.
+	 */
+	NONE;
+	
+	public boolean equals(SessionFixationPolicy policy) {
+		return this.compareTo(policy) == 0;
+	}
+
+}
