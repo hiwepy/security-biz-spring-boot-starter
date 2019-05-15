e957f7102238b6e31e9a9392b33d595b6cb4307e
wandl <hnxyhcwdl1003@163.com>
2019-05-15 18:24:52 +0800

    优化

--- src/main/java/org/springframework/security/boot/biz/exception/AuthenticationMethodNotSupportedException.java
+++ src/main/java/org/springframework/security/boot/biz/exception/AuthenticationMethodNotSupportedException.java
@@ -1,17 +1,34 @@
 package org.springframework.security.boot.biz.exception;
 
-import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
 
 /**
- * 
- * @author vladimir.stankovic
- *
- * Aug 4, 2016
  */
-public class AuthenticationMethodNotSupportedException extends AuthenticationServiceException {
-    private static final long serialVersionUID = 3705043083010304496L;
+@SuppressWarnings("serial")
+public class AuthenticationMethodNotSupportedException extends AuthenticationException {
+	
+	// ~ Constructors
+	// ===================================================================================================
 
-    public AuthenticationMethodNotSupportedException(String msg) {
-        super(msg);
-    }
+	/**
+	 * Constructs an <code>AuthenticationMethodNotSupportedException</code> with the
+	 * specified message.
+	 *
+	 * @param msg the detail message
+	 */
+	public AuthenticationMethodNotSupportedException(String msg) {
+		super(msg);
+	}
+
+	/**
+	 * Constructs an <code>AuthenticationMethodNotSupportedException</code> with the
+	 * specified message and root cause.
+	 *
+	 * @param msg the detail message
+	 * @param t root cause
+	 */
+	public AuthenticationMethodNotSupportedException(String msg, Throwable t) {
+		super(msg, t);
+	}
+	
 }
