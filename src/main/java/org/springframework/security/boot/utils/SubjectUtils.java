f4d425525ec37b4b26bf03d3ef33a640e2cbf080
wandl <hnxyhcwdl1003@163.com>
2020-03-31 00:24:47 +0800

    xg

--- src/main/java/org/springframework/security/boot/utils/SubjectUtils.java
+++ src/main/java/org/springframework/security/boot/utils/SubjectUtils.java
@@ -1,6 +1,8 @@
 package org.springframework.security.boot.utils;
 
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -8,16 +10,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.web.servlet.server.Session;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.security.boot.biz.SpringSecurityBizMessageSource;
+import org.springframework.security.boot.biz.exception.AuthResponseCode;
+import org.springframework.security.boot.biz.userdetails.SecurityPrincipal;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.context.request.ServletWebRequest;
 
 public class SubjectUtils {
-
+	
+	private static MessageSourceAccessor messages = SpringSecurityBizMessageSource.getAccessor();
+	private static final String EMPTY = "null";
+	
 	public static SecurityContext getSecurityContext(){
 		return SecurityContextHolder.getContext();
 	}
@@ -110,6 +122,57 @@ public class SubjectUtils {
 			newSession.setAttribute(String.valueOf(key), attributes.get(key));
 		}
 		return newSession;
+	}
+	
+	public static Map<String, Object> tokenMap(Authentication authentication, String token){
+		
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		
+		Map<String, Object> tokenMap = new HashMap<String, Object>();
+		
+		tokenMap.put("code", AuthResponseCode.SC_AUTHC_SUCCESS.getCode());
+		tokenMap.put("msg", messages.getMessage(AuthResponseCode.SC_AUTHC_SUCCESS.getMsgKey()));
+		tokenMap.put("status", "success");
+		
+		// 账号首次登陆标记
+		if(SecurityPrincipal.class.isAssignableFrom(userDetails.getClass())) {
+			SecurityPrincipal securityPrincipal = (SecurityPrincipal) userDetails;
+			tokenMap.put("initial", securityPrincipal.isInitial());
+			tokenMap.put("alias", StringUtils.defaultString(securityPrincipal.getAlias(), EMPTY));
+			tokenMap.put("userid", StringUtils.defaultString(securityPrincipal.getUserid(), EMPTY));
+			tokenMap.put("userkey", StringUtils.defaultString(securityPrincipal.getUserkey(), EMPTY));
+			tokenMap.put("usercode", StringUtils.defaultString(securityPrincipal.getUsercode(), EMPTY));
+			tokenMap.put("username", userDetails.getUsername());
+			tokenMap.put("perms", userDetails.getAuthorities());
+			tokenMap.put("roleid", StringUtils.defaultString(securityPrincipal.getRoleid(), EMPTY ));
+			tokenMap.put("role", StringUtils.defaultString(securityPrincipal.getRole(), EMPTY));
+			tokenMap.put("roles", CollectionUtils.isEmpty(securityPrincipal.getRoles()) ? new ArrayList<>() : securityPrincipal.getRoles() );
+			tokenMap.put("restricted", securityPrincipal.isRestricted());
+			tokenMap.put("profile", CollectionUtils.isEmpty(securityPrincipal.getProfile()) ? new HashMap<>() : securityPrincipal.getProfile() );
+			tokenMap.put("faced", securityPrincipal.isFace());
+			tokenMap.put("faceId", StringUtils.defaultString(securityPrincipal.getFaceId(), EMPTY ));
+			// JSON Web Token (JWT)
+			tokenMap.put("token", token);
+		} else {
+			tokenMap.put("initial", false);
+			tokenMap.put("alias", "匿名账户");
+			tokenMap.put("userid", EMPTY);
+			tokenMap.put("userkey", EMPTY);
+			tokenMap.put("usercode", EMPTY);
+			tokenMap.put("username", EMPTY);
+			tokenMap.put("perms", new ArrayList<>());
+			tokenMap.put("roleid", EMPTY);
+			tokenMap.put("role", EMPTY);
+			tokenMap.put("roles", new ArrayList<>());
+			tokenMap.put("restricted", false);
+			tokenMap.put("profile", new HashMap<>());
+			tokenMap.put("faced", false);
+			tokenMap.put("faceId", EMPTY);
+			tokenMap.put("token", EMPTY);
+		}
+		
+		return tokenMap;
+		
 	}
 	
 }
