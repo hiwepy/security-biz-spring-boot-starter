2691e43cc25ce7a0ea1b360cb5114689a13147b5
wandl <hiwepy@163.com>
2021-04-15 14:45:46 +0800

    yh

---
+++ src/main/java/org/springframework/security/boot/biz/authentication/AbstractAuthenticationToken.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.security.boot.biz.authentication;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@SuppressWarnings("serial")
+public class AbstractAuthenticationToken extends org.springframework.security.authentication.AbstractAuthenticationToken {
+    
+    private final Object principal;
+    private Object credentials;
+    /**
+	 * 登录的用户UID
+	 */
+	private String uid;
+	/**
+	 * 登录的客户端应用ID
+	 */
+	private String appId;
+	/**
+	 * 登录的客户端应用渠道编码
+	 */
+	private String appChannel;
+	/**
+	 *登录的客户端版本
+	 */
+	private String appVersion;
+	/**
+	 * 请求参数签名（可选）
+	 */
+	private String sign;
+	/**
+	 * 用户最新经度（可选）
+	 */
+	private double longitude;
+	/**
+	 * 用户最新纬度（可选）
+	 */
+	private double latitude;
+
+    public AbstractAuthenticationToken(Object principal) {
+        super(null);
+        this.principal = principal;
+        setAuthenticated(false);
+    }
+    
+    public AbstractAuthenticationToken( Object principal,  Object credentials, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.principal = principal;
+        this.credentials = credentials;
+        super.setAuthenticated(true); // must use super, as we override
+    }
+
+    // ~ Methods
+    // ========================================================================================================
+
+    public Object getCredentials() {
+        return this.credentials;
+    }
+
+    public Object getPrincipal() {
+        return this.principal;
+    }
+
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+        if (isAuthenticated) {
+            throw new IllegalArgumentException(
+                    "Cannot set this token to trusted - use constructor which takes a GrantedAuthority list instead");
+        }
+
+        super.setAuthenticated(false);
+    }
+
+    @Override
+    public void eraseCredentials() {
+        super.eraseCredentials();
+        credentials = null;
+    }
+    
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getAppId() {
+		return appId;
+	}
+
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+
+	public String getAppChannel() {
+		return appChannel;
+	}
+
+	public void setAppChannel(String appChannel) {
+		this.appChannel = appChannel;
+	}
+
+	public String getAppVersion() {
+		return appVersion;
+	}
+
+	public void setAppVersion(String appVersion) {
+		this.appVersion = appVersion;
+	}
+
+	public String getSign() {
+		return sign;
+	}
+
+	public void setSign(String sign) {
+		this.sign = sign;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+}
