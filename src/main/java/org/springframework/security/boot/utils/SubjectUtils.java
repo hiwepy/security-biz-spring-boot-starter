156c15a617a32214d4d2ea14fd63544892b5d85b
wandl <hiwepy@163.com>
2022-04-26 14:40:43 +0800

    yh

--- src/main/java/org/springframework/security/boot/utils/SubjectUtils.java
+++ src/main/java/org/springframework/security/boot/utils/SubjectUtils.java
@@ -1,8 +1,11 @@
 package org.springframework.security.boot.utils;
 
+import java.math.BigDecimal;
 import java.util.Enumeration;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,89 +27,107 @@ import org.springframework.web.context.request.ServletWebRequest;
  */
 @SuppressWarnings("unchecked")
 public class SubjectUtils {
-	
+
+	public static final Function<Object, Long> TO_LONG = member -> {
+		if(Objects.isNull(member)) {
+			return null;
+		}
+		return member instanceof Long ? (Long) member : new BigDecimal(member.toString()).longValue();
+	};
+
 	public static SecurityContext getSecurityContext(){
 		return SecurityContextHolder.getContext();
 	}
-	
+
 	public static Authentication getAuthentication(){
 		return getSecurityContext().getAuthentication();
 	}
-	
+
 	public static <T> T getPrincipal(Class<T> clazz){
 		Object principal = getAuthentication().getPrincipal();
-		// 自身类.class.isAssignableFrom(自身类或子类.class) 
+		// 自身类.class.isAssignableFrom(自身类或子类.class)
 		if( clazz.isAssignableFrom(principal.getClass()) ) {
 			return (T)principal;
 		}
 		return null;
 	}
-	
+
 	public static <T> T getPrincipal(Authentication authentication, Class<T> clazz){
 		Object principal = authentication.getPrincipal();
-		// 自身类.class.isAssignableFrom(自身类或子类.class) 
+		// 自身类.class.isAssignableFrom(自身类或子类.class)
 		if( clazz.isAssignableFrom(principal.getClass()) ) {
 			return (T)principal;
 		}
 		return null;
 	}
-	
-	public static String getProfileString(Authentication authentication, String key) {
-		SecurityPrincipal principal = SubjectUtils.getPrincipal(authentication, SecurityPrincipal.class);
-		return MapUtils.getString(principal.getProfile(), key);
-	}
-	
-	public static int getProfileInt(Authentication authentication, String key) {
-		SecurityPrincipal principal = SubjectUtils.getPrincipal(authentication, SecurityPrincipal.class);
-		return MapUtils.getIntValue(principal.getProfile(), key);
-	}
-	
-	public static Double getProfileDouble(Authentication authentication, String key) {
-		SecurityPrincipal principal = SubjectUtils.getPrincipal(authentication, SecurityPrincipal.class);
-		return MapUtils.getDouble(principal.getProfile(), key);
-	}
-	
+
 	public static Object getPrincipal(){
 		Authentication authentication = getAuthentication();
 		return authentication == null ? null : authentication.getPrincipal();
 	}
-	
+
+	public static String getUserId() {
+		SecurityPrincipal principal = SubjectUtils.getPrincipal(SecurityPrincipal.class);
+		return principal.getUid();
+	}
+
+	public static Long getUserIdLong() {
+		SecurityPrincipal principal = SubjectUtils.getPrincipal(SecurityPrincipal.class);
+		return TO_LONG.apply(principal.getUid());
+	}
+
+	public static String getProfileString(Authentication authentication, String key) {
+		SecurityPrincipal principal = SubjectUtils.getPrincipal(authentication, SecurityPrincipal.class);
+		return MapUtils.getString(principal.getProfile(), key);
+	}
+
+	public static int getProfileInt(Authentication authentication, String key) {
+		SecurityPrincipal principal = SubjectUtils.getPrincipal(authentication, SecurityPrincipal.class);
+		return MapUtils.getIntValue(principal.getProfile(), key);
+	}
+
+	public static Double getProfileDouble(Authentication authentication, String key) {
+		SecurityPrincipal principal = SubjectUtils.getPrincipal(authentication, SecurityPrincipal.class);
+		return MapUtils.getDouble(principal.getProfile(), key);
+	}
+
+
 	public static String getProfileString(String key) {
 		SecurityPrincipal principal = SubjectUtils.getPrincipal( SecurityPrincipal.class);
 		return MapUtils.getString(principal.getProfile(), key);
 	}
-	
+
 	public static int getProfileInt(String key) {
 		SecurityPrincipal principal = SubjectUtils.getPrincipal( SecurityPrincipal.class);
 		return MapUtils.getIntValue(principal.getProfile(), key);
 	}
-	
+
 	public static Double getProfileDouble(String key) {
 		SecurityPrincipal principal = SubjectUtils.getPrincipal( SecurityPrincipal.class);
 		return MapUtils.getDouble(principal.getProfile(), key);
 	}
-	
+
 	public static boolean isAuthenticated(){
 		Authentication authentication = getAuthentication();
 		return authentication == null ? false : authentication.isAuthenticated();
 	}
-	
+
 	public static ServletRequestAttributes getRequestAttributes() {
-		return (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();   
+		return (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
 	}
-	
+
 	public static HttpServletRequest getRequest() {
-		return getRequestAttributes().getRequest();   
+		return getRequestAttributes().getRequest();
 	}
-	 
+
 	public static HttpServletResponse getResponse() {
 		return ((ServletWebRequest)RequestContextHolder.getRequestAttributes()).getResponse();
 	}
-	
+
 	public static HttpSession getSession(boolean create){
 		return getRequest().getSession(create);
 	}
-	
+
 	/*
 	 * 检查target类型对是否是给出对象类型数组中任意一个的类型的子类或者子接口
 	 */
@@ -115,8 +136,8 @@ public class SubjectUtils {
 			for (Class<?> clazz : classes) {
 				/*
 				 *	假设有两个类Class1和Class2。Class1.isAssignableFrom(Class2)表示:
-    			 *	1、类Class1和Class2是否相同。
-    			 *	2、Class1是否是Class2的父类或接口 
+				 *	1、类Class1和Class2是否相同。
+				 *	2、Class1是否是Class2的父类或接口
 				 */
 				// clazz是否和target类型相同或者，clazz是否是target的父类或接口 
 				if(clazz != null && clazz.isAssignableFrom(target)) {
@@ -126,7 +147,7 @@ public class SubjectUtils {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * 登陆成功后重新生成session【基于安全考虑】
 	 * @param request {@link HttpServletRequest} instance
@@ -153,5 +174,5 @@ public class SubjectUtils {
 		}
 		return newSession;
 	}
-	
+
 }
